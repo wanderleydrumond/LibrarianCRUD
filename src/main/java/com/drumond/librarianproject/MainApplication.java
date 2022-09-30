5bd1c017e45eaee22ffc879150110e14a186f405
Wanderley Drumond <wanderley.drumond@gmail.com>
2022-09-30 18:02:48 +0100

    Application icon implemented

--- src/main/java/com/drumond/librarianproject/MainApplication.java
+++ src/main/java/com/drumond/librarianproject/MainApplication.java
@@ -3,6 +3,7 @@ package com.drumond.librarianproject;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class MainApplication extends Application {
@@ -10,6 +11,7 @@ public class MainApplication extends Application {
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 266);
+        stage.getIcons().add(new Image(MainApplication.class.getResourceAsStream("/images/books.png")));
         stage.setResizable(false);
         stage.setTitle("The Librarian Project");
         stage.setScene(scene);
