21be5e36ace5c577d975b93724394e2f33034e03
Wanderley Drumond <wanderley.drumond@gmail.com>
2022-10-06 14:56:42 +0100

    - App Lomboked

--- src/main/java/com/drumond/librarianproject/Books.java
+++ src/main/java/com/drumond/librarianproject/Books.java
@@ -1,37 +1,14 @@
 package com.drumond.librarianproject;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class Books {
     private final int id;
     private final String title;
     private final String author;
     private final int year;
     private final int pages;
-
-    public Books(int id, String title, String author, int year, int pages) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-        this.year = year;
-        this.pages = pages;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public int getPages() {
-        return pages;
-    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.drumond.librarianproject {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires static lombok;
 
     opens com.drumond.librarianproject to javafx.fxml;
     exports com.drumond.librarianproject;
